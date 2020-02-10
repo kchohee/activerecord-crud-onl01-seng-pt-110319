519b8ee27e7b4e25d7a6f71a916c016de92d9756
Marie (Cho) Hughes <kchohee0630@gmail.com>
2020-02-10 22:34:59 +0000

    Automatically backed up by Learn

--- db/migrate/20200210222603_create_movies.rb
+++ db/migrate/20200210222603_create_movies.rb
@@ -1,5 +1,6 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
-  def changecreate_table :movies do |t| 
+  def change
+    create_table :movies do |t| 
     t.string :title
     t.integer :release_date
     t.string :director
